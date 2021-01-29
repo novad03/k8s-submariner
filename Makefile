pinikomarov/submariner

Support building non-native images
This builds on top of the buildx-based multiarch support to allow
building non-native, and ultimately multiarch, images:

    make ARCHES=arm images

will build images for 32-bit ARM (if the base images support that),
and

    make ARCHES=arm,arm64,amd64 images

will build multiarch images for all three architectures once Shipyard
supports that.

Signed-off-by: Stephen Kitt <skitt@redhat.com>

Makefile
@@ -30,6 +30,22 @@ override DEPLOY_ARGS += --deploytool_submariner_args '--cable-driver $(cable_dri
 endif
 endif
 
+# When cross-building, we need to map Go architectures and operating systems to Docker buildx platforms:
+# Docker buildx platform | Fedora support? | Go
+# --------------------------------------------------
+# linux/amd64            | Yes (amd64)     | linux/amd64
+# linux/arm64            | Yes (arm64v8)   | linux/arm64
+# linux/riscv64          | No              | linux/riscv64
+# linux/ppc64le          | Yes (ppc64le)   | linux/ppc64le
+# linux/s390x            | Yes (s390x)     | linux/s390x
+# linux/386              | No              | linux/386
+# linux/arm/v7           | Yes (arm32v7)   | linux/arm
+# linux/arm/v6           | No              | N/A
+#
+# References: https://github.com/golang/go/blob/master/src/go/build/syslist.go
+gotodockerarch = $(patsubst arm,arm/v7,$(1))
+dockertogoarch = $(patsubst arm/v7,arm,$(1))
+
 # Targets to make
 
 deploy: images
@@ -37,19 +53,30 @@ deploy: images
 reload-images: build images
 	./scripts/$@ --restart $(restart)
 
-bin/submariner-engine: vendor/modules.txt main.go $(shell find pkg -not \( -path 'pkg/globalnet*' -o -path 'pkg/routeagent*' \))
-	${SCRIPTS_DIR}/compile.sh $@ main.go $(BUILD_ARGS)
+bin/%/submariner-engine: vendor/modules.txt main.go $(shell find pkg -not \( -path 'pkg/globalnet*' -o -path 'pkg/routeagent*' \))
+	GOARCH=$(call dockertogoarch,$(patsubst bin/linux/%/,%,$(dir $@))) ${SCRIPTS_DIR}/compile.sh $@ main.go $(BUILD_ARGS)
 
-bin/submariner-route-agent: vendor/modules.txt $(shell find pkg/routeagent_driver)
-	${SCRIPTS_DIR}/compile.sh $@ ./pkg/routeagent_driver/main.go $(BUILD_ARGS)
+bin/%/submariner-route-agent: vendor/modules.txt $(shell find pkg/routeagent)
+	GOARCH=$(call dockertogoarch,$(patsubst bin/linux/%/,%,$(dir $@))) ${SCRIPTS_DIR}/compile.sh $@ ./pkg/routeagent/main.go $(BUILD_ARGS)
 
-bin/submariner-globalnet: vendor/modules.txt $(shell find pkg/globalnet)
-	${SCRIPTS_DIR}/compile.sh $@ ./pkg/globalnet/main.go $(BUILD_ARGS)
+bin/%/submariner-globalnet: vendor/modules.txt $(shell find pkg/globalnet)
+	GOARCH=$(call dockertogoarch,$(patsubst bin/linux/%/,%,$(dir $@))) ${SCRIPTS_DIR}/compile.sh $@ ./pkg/globalnet/main.go $(BUILD_ARGS)
 
-bin/submariner-networkplugin-syncer: vendor/modules.txt $(shell find pkg/networkplugin-syncer)
-	${SCRIPTS_DIR}/compile.sh $@ ./pkg/networkplugin-syncer/main.go $(BUILD_ARGS)
+bin/%/submariner-networkplugin-syncer: vendor/modules.txt $(shell find pkg/networkplugin-syncer)
+	GOARCH=$(call dockertogoarch,$(patsubst bin/linux/%/,%,$(dir $@))) ${SCRIPTS_DIR}/compile.sh $@ ./pkg/networkplugin-syncer/main.go $(BUILD_ARGS)
 
-build: bin/submariner-engine bin/submariner-route-agent bin/submariner-globalnet bin/submariner-networkplugin-syncer
+nullstring :=
+space := $(nullstring) # end of the line
+comma := ,
+
+# Single-architecture only for now (we need to support manifests in Shipyard)
+# This can be overridden to build for other supported architectures; the reference is the Go architecture,
+# so "make images ARCHES=arm" will build a linux/arm/v7 image
+ARCHES ?= amd64
+BINARIES = submariner-engine submariner-route-agent submariner-globalnet submariner-networkplugin-syncer
+IMAGES_ARGS = --platform $(subst $(space),$(comma),$(foreach arch,$(subst $(comma),$(space),$(ARCHES)),linux/$(call gotodockerarch,$(arch))))
+
+build: $(foreach arch,$(subst $(comma),$(space),$(ARCHES)),$(foreach binary,$(BINARIES),bin/linux/$(call gotodockerarch,$(arch))/$(binary)))
 
 ci: validate unit build images
 

novad03/k8s-submariner

Makefile
@@ -14,7 +14,21 @@ override E2E_ARGS += --focus $(focus) cluster2 cluster3 cluster1
 override UNIT_TEST_ARGS += test/e2e
 override VALIDATE_ARGS += --skip-dirs pkg/client
 
-# Process extra flags from the `using=a,b,c` optional flag
+# When cross-building, we need to map Go architectures and operating systems to Docker buildx platforms:
+# Docker buildx platform | Fedora support? | Go
+# --------------------------------------------------
+# linux/amd64            | Yes (amd64)     | linux/amd64
+# linux/arm64            | Yes (arm64v8)   | linux/arm64
+# linux/riscv64          | No              | linux/riscv64
+# linux/ppc64le          | Yes (ppc64le)   | linux/ppc64le
+# linux/s390x            | Yes (s390x)     | linux/s390x
+# linux/386              | No              | linux/386
+# linux/arm/v7           | Yes (arm32v7)   | linux/arm
+# linux/arm/v6           | No              | N/A
+#
+# References: https://github.com/golang/go/blob/master/src/go/build/syslist.go
+gotodockerarch = $(patsubst arm,arm/v7,$(1))
+dockertogoarch = $(patsubst arm/v7,arm,$(1))
 
 # Targets to make
 
@@ -23,19 +37,30 @@ deploy: images
 reload-images: build images
 	./scripts/$@ --restart $(restart)
 
-bin/submariner-engine: vendor/modules.txt main.go $(shell find pkg -not \( -path 'pkg/globalnet*' -o -path 'pkg/routeagent*' \))
-	${SCRIPTS_DIR}/compile.sh $@ main.go $(BUILD_ARGS)
+bin/%/submariner-engine: vendor/modules.txt main.go $(shell find pkg -not \( -path 'pkg/globalnet*' -o -path 'pkg/routeagent*' \))
+	GOARCH=$(call dockertogoarch,$(patsubst bin/linux/%/,%,$(dir $@))) ${SCRIPTS_DIR}/compile.sh $@ main.go $(BUILD_ARGS)
 
-bin/submariner-route-agent: vendor/modules.txt $(shell find pkg/routeagent_driver)
-	${SCRIPTS_DIR}/compile.sh $@ ./pkg/routeagent_driver/main.go $(BUILD_ARGS)
+bin/%/submariner-route-agent: vendor/modules.txt $(shell find pkg/routeagent_driver)
+	GOARCH=$(call dockertogoarch,$(patsubst bin/linux/%/,%,$(dir $@))) ${SCRIPTS_DIR}/compile.sh $@ ./pkg/routeagent_driver/main.go $(BUILD_ARGS)
 
-bin/submariner-globalnet: vendor/modules.txt $(shell find pkg/globalnet)
-	${SCRIPTS_DIR}/compile.sh $@ ./pkg/globalnet/main.go $(BUILD_ARGS)
+bin/%/submariner-globalnet: vendor/modules.txt $(shell find pkg/globalnet)
+	GOARCH=$(call dockertogoarch,$(patsubst bin/linux/%/,%,$(dir $@))) ${SCRIPTS_DIR}/compile.sh $@ ./pkg/globalnet/main.go $(BUILD_ARGS)
 
-bin/submariner-networkplugin-syncer: vendor/modules.txt $(shell find pkg/networkplugin-syncer)
-	${SCRIPTS_DIR}/compile.sh $@ ./pkg/networkplugin-syncer/main.go $(BUILD_ARGS)
+bin/%/submariner-networkplugin-syncer: vendor/modules.txt $(shell find pkg/networkplugin-syncer)
+	GOARCH=$(call dockertogoarch,$(patsubst bin/linux/%/,%,$(dir $@))) ${SCRIPTS_DIR}/compile.sh $@ ./pkg/networkplugin-syncer/main.go $(BUILD_ARGS)
 
-build: bin/submariner-engine bin/submariner-route-agent bin/submariner-globalnet bin/submariner-networkplugin-syncer
+nullstring :=
+space := $(nullstring) # end of the line
+comma := ,
+
+# Single-architecture only for now (we need to support manifests in Shipyard)
+# This can be overridden to build for other supported architectures; the reference is the Go architecture,
+# so "make images ARCHES=arm" will build a linux/arm/v7 image
+ARCHES ?= amd64
+BINARIES = submariner-engine submariner-route-agent submariner-globalnet submariner-networkplugin-syncer
+IMAGES_ARGS = --platform $(subst $(space),$(comma),$(foreach arch,$(subst $(comma),$(space),$(ARCHES)),linux/$(call gotodockerarch,$(arch))))
+
+build: $(foreach arch,$(subst $(comma),$(space),$(ARCHES)),$(foreach binary,$(BINARIES),bin/linux/$(call gotodockerarch,$(arch))/$(binary)))
 
 ci: validate unit build images
 