skitt/submariner

Use k8s.io generic sets instead of Admiral stringset
We rely on k8s.io/apimachinery/pkg/util anyway; using the k8s.io
generic sets means one less piece of code to maintain.

Signed-off-by: Stephen Kitt <skitt@redhat.com>

pkg/routeagent_driver/handlers/ovn/gateway_dataplane.go
@@ -36,7 +36,7 @@ func (ovn *Handler) cleanupGatewayDataplane() error {
 		return errors.Wrapf(err, "error reading ip rule list for IPv4")
 	}
 
-	err = ovn.handleSubnets(currentRemoteSubnets.Elements(), netlink.RuleDel, os.IsNotExist)
+	err = ovn.handleSubnets(currentRemoteSubnets.UnsortedList(), netlink.RuleDel, os.IsNotExist)
 	if err != nil {
 		return errors.Wrapf(err, "error removing routing rule")
 	}
@@ -57,14 +57,14 @@ func (ovn *Handler) updateGatewayDataplane() error {
 
 	endpointSubnets := ovn.getRemoteSubnets()
 
-	toAdd := currentRuleRemotes.Difference(endpointSubnets)
+	toAdd := currentRuleRemotes.Difference(endpointSubnets).UnsortedList()
 
 	err = ovn.handleSubnets(toAdd, netlink.RuleAdd, os.IsExist)
 	if err != nil {
 		return errors.Wrap(err, "error adding routing rule")
 	}
 
-	toRemove := endpointSubnets.Difference(currentRuleRemotes)
+	toRemove := endpointSubnets.Difference(currentRuleRemotes).UnsortedList()
 
 	err = ovn.handleSubnets(toRemove, netlink.RuleDel, os.IsNotExist)
 	if err != nil {
@@ -110,7 +110,7 @@ func (ovn *Handler) getMSSClampingRuleSpecs() ([][]string, error) {
 	//   * https://github.com/submariner-io/submariner/issues/1488
 	// TODO: get the kernel to steer the ICMPs back to ovn-k8s-sub0 interface properly, or write a packet
 	//       reflector in the route agent for that type of packets
-	for _, remoteCIDR := range ovn.getRemoteSubnets().Elements() {
+	for _, remoteCIDR := range ovn.getRemoteSubnets().UnsortedList() {
 		rules = append(rules,
 			[]string{
 				"-d", remoteCIDR, "-p", "tcp", "-m", "tcp",

pkg/routeagent_driver/handlers/ovn/host_networking.go
@@ -26,9 +26,9 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/submariner-io/admiral/pkg/log"
-	"github.com/submariner-io/admiral/pkg/stringset"
 	"github.com/submariner-io/submariner/pkg/routeagent_driver/constants"
 	"github.com/vishvananda/netlink"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 const (
@@ -43,14 +43,14 @@ func (ovn *Handler) updateHostNetworkDataplane() error {
 
 	endpointSubnets := ovn.getRemoteSubnets()
 
-	toAdd := currentRuleRemotes.Difference(endpointSubnets)
+	toAdd := currentRuleRemotes.Difference(endpointSubnets).UnsortedList()
 
 	err = ovn.programRulesForRemoteSubnets(toAdd, netlink.RuleAdd, os.IsExist)
 	if err != nil {
 		return errors.Wrap(err, "error adding routing rule")
 	}
 
-	toRemove := endpointSubnets.Difference(currentRuleRemotes)
+	toRemove := endpointSubnets.Difference(currentRuleRemotes).UnsortedList()
 
 	err = ovn.programRulesForRemoteSubnets(toRemove, netlink.RuleDel, os.IsNotExist)
 	if err != nil {
@@ -75,8 +75,8 @@ func (ovn *Handler) updateHostNetworkDataplane() error {
 	return nil
 }
 
-func (ovn *Handler) getExistingIPv4HostNetworkRoutes() (stringset.Interface, error) {
-	currentRuleRemotes := stringset.New()
+func (ovn *Handler) getExistingIPv4HostNetworkRoutes() (sets.Set[string], error) {
+	currentRuleRemotes := sets.New[string]()
 
 	rules, err := netlink.RuleList(netlink.FAMILY_V4)
 	if err != nil {
@@ -85,7 +85,7 @@ func (ovn *Handler) getExistingIPv4HostNetworkRoutes() (stringset.Interface, err
 
 	for i := range rules {
 		if rules[i].Table == constants.RouteAgentHostNetworkTableID && rules[i].Dst != nil {
-			currentRuleRemotes.Add(rules[i].Dst.String())
+			currentRuleRemotes.Insert(rules[i].Dst.String())
 		}
 	}
 

novad03/k8s-submariner

pkg/routeagent_driver/handlers/ovn/gateway_dataplane.go
@@ -36,7 +36,7 @@ func (ovn *Handler) cleanupGatewayDataplane() error {
 		return errors.Wrapf(err, "error reading ip rule list for IPv4")
 	}
 
-	err = ovn.handleSubnets(currentRemoteSubnets.Elements(), netlink.RuleDel, os.IsNotExist)
+	err = ovn.handleSubnets(currentRemoteSubnets.UnsortedList(), netlink.RuleDel, os.IsNotExist)
 	if err != nil {
 		return errors.Wrapf(err, "error removing routing rule")
 	}
@@ -57,14 +57,14 @@ func (ovn *Handler) updateGatewayDataplane() error {
 
 	endpointSubnets := ovn.getRemoteSubnets()
 
-	toAdd := currentRuleRemotes.Difference(endpointSubnets)
+	toAdd := endpointSubnets.Difference(currentRuleRemotes).UnsortedList()
 
 	err = ovn.handleSubnets(toAdd, netlink.RuleAdd, os.IsExist)
 	if err != nil {
 		return errors.Wrap(err, "error adding routing rule")
 	}
 
-	toRemove := endpointSubnets.Difference(currentRuleRemotes)
+	toRemove := currentRuleRemotes.Difference(endpointSubnets).UnsortedList()
 
 	err = ovn.handleSubnets(toRemove, netlink.RuleDel, os.IsNotExist)
 	if err != nil {
@@ -110,7 +110,7 @@ func (ovn *Handler) getMSSClampingRuleSpecs() ([][]string, error) {
 	//   * https://github.com/submariner-io/submariner/issues/1488
 	// TODO: get the kernel to steer the ICMPs back to ovn-k8s-sub0 interface properly, or write a packet
 	//       reflector in the route agent for that type of packets
-	for _, remoteCIDR := range ovn.getRemoteSubnets().Elements() {
+	for _, remoteCIDR := range ovn.getRemoteSubnets().UnsortedList() {
 		rules = append(rules,
 			[]string{
 				"-d", remoteCIDR, "-p", "tcp", "-m", "tcp",

pkg/routeagent_driver/handlers/ovn/host_networking.go
@@ -26,9 +26,9 @@ import (
 
 	"github.com/pkg/errors"
 	"github.com/submariner-io/admiral/pkg/log"
-	"github.com/submariner-io/admiral/pkg/stringset"
 	"github.com/submariner-io/submariner/pkg/routeagent_driver/constants"
 	"github.com/vishvananda/netlink"
+	"k8s.io/apimachinery/pkg/util/sets"
 )
 
 const (
@@ -43,14 +43,14 @@ func (ovn *Handler) updateHostNetworkDataplane() error {
 
 	endpointSubnets := ovn.getRemoteSubnets()
 
-	toAdd := currentRuleRemotes.Difference(endpointSubnets)
+	toAdd := endpointSubnets.Difference(currentRuleRemotes).UnsortedList()
 
 	err = ovn.programRulesForRemoteSubnets(toAdd, netlink.RuleAdd, os.IsExist)
 	if err != nil {
 		return errors.Wrap(err, "error adding routing rule")
 	}
 
-	toRemove := endpointSubnets.Difference(currentRuleRemotes)
+	toRemove := currentRuleRemotes.Difference(endpointSubnets).UnsortedList()
 
 	err = ovn.programRulesForRemoteSubnets(toRemove, netlink.RuleDel, os.IsNotExist)
 	if err != nil {
@@ -75,8 +75,8 @@ func (ovn *Handler) updateHostNetworkDataplane() error {
 	return nil
 }
 
-func (ovn *Handler) getExistingIPv4HostNetworkRoutes() (stringset.Interface, error) {
-	currentRuleRemotes := stringset.New()
+func (ovn *Handler) getExistingIPv4HostNetworkRoutes() (sets.Set[string], error) {
+	currentRuleRemotes := sets.New[string]()
 
 	rules, err := netlink.RuleList(netlink.FAMILY_V4)
 	if err != nil {
@@ -85,7 +85,7 @@ func (ovn *Handler) getExistingIPv4HostNetworkRoutes() (stringset.Interface, err
 
 	for i := range rules {
 		if rules[i].Table == constants.RouteAgentHostNetworkTableID && rules[i].Dst != nil {
-			currentRuleRemotes.Add(rules[i].Dst.String())
+			currentRuleRemotes.Insert(rules[i].Dst.String())
 		}
 	}
 